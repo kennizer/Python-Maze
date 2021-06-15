9cb21ebdd1444fb3244f8f7e358d6b69413a542d
kennizer <53318888+kennizer@users.noreply.github.com>
2021-06-14 22:22:01 -0400

    Added text and endpoint now
    Added text that auto updates itself as well as an endpoint .
    
    Will need to work on how to get the text to wrap around tomorrow

--- maze.py
+++ maze.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random,textwrap
 class Direction: 
     UP = 1 
     DOWN = -1
@@ -158,42 +158,76 @@ end_x, end_y = cell_x-1, cell_y-1
 
 steps, maze = generate_maze((start_x,start_y), (end_x,end_y), cell_x, cell_y)
 
-colors = [(255,0,0), (0,255,0), (0,0,255)]
+colors = [(255,0,0), (0,255,0), (0,0,255), (0,0,0)]
 erase = (255,255,255)
 width = 500     
 height = 500 
+
 additional_x = 50
 additional_y = 50
+total_width = width + additional_x * 2 
+total_height = height + additional_y*2
 thickness = 5
 radius = 7
+top_font_size = 32
+bottom_font_size = 20 
+top_text = "A " + str(cell_x) + " x "+ str(cell_y)+ " Maze"
+bottom_text = "Navigate from the red dot to the blue dot"
 
 x_size = width/ cell_x
 y_size = height/ cell_y
 
+suggestion = "Want to speed things up? Press + to solve the puzzle."
+inspiration_iterations = 0 
+timer = 5000
+inspiration_font_size = 16
+inspirational_file = open('quotes.txt', 'r')
+lines = inspirational_file.readlines() 
+inspiration = []
+for l in lines: 
+    inspiration.append(l.strip())
+
 pygame.init() 
-screen = pygame.display.set_mode((width + additional_x*2, height + additional_y*2))
+screen = pygame.display.set_mode((total_width, total_height))
 screen.fill(erase)
 
+top_font = pygame.font.Font('freesansbold.ttf', top_font_size)
+bottom_font = pygame.font.Font('freesansbold.ttf', bottom_font_size)
+inspiration_font = pygame.font.Font('freesansbold.ttf', inspiration_font_size)
+top_box = top_font.render(top_text, True, colors[3], erase)
+top_rectangle = top_box.get_rect()
+top_rectangle.center = (total_width/2, additional_y/2)
+bottom_box = bottom_font.render(bottom_text, True, colors[3], erase)
+bottom_rectangle = bottom_box.get_rect()
+bottom_rectangle.center = (total_width/2, total_height-additional_y/2)
+inspiration_rectangle = (0, total_height-additional_y/2)
+last_font = bottom_font
+last_rectangle = bottom_rectangle 
 draw_grid (x_size, y_size, cell_x, cell_y, screen, thickness, additional_x, additional_y)
 draw_maze(steps,x_size,y_size,screen,thickness,additional_x, additional_y)
 
 up_start = Direction.UP in maze[start_x][start_y].directions or Direction.DOWN in maze[start_x][start_y].directions
 up_end = Direction.UP in maze[end_x][end_y].directions or Direction.DOWN in maze[end_x][end_y].directions
-draw_square(start_x,start_y,x_size,y_size,colors[0],screen,thickness,additional_x, additional_y, up_start)
-draw_square(end_x,end_y,x_size,y_size,colors[2],screen,thickness,additional_x, additional_y, up_end)
 
 circle_x, circle_y = start_x,start_y
+
+draw_square(start_x,start_y,x_size,y_size,colors[0],screen,thickness,additional_x, additional_y, up_start)
+draw_square(end_x,end_y,x_size,y_size,colors[2],screen,thickness,additional_x, additional_y, up_end)
 draw_circle(circle_x,circle_y, x_size,y_size, radius, colors[1], screen, 0, additional_x,additional_y)
-pressed = False 
+screen.blit(top_box,top_rectangle)
+screen.blit(bottom_box,bottom_rectangle)
+
+pygame.time.set_timer(pygame.USEREVENT, timer)
+
+objective_achieved = False
 while True: 
-    
     draw_square(start_x,start_y,x_size,y_size,colors[0],screen,thickness,additional_x, additional_y, up_start)
     draw_square(end_x,end_y,x_size,y_size,colors[2],screen,thickness,additional_x, additional_y, up_end)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and not objective_achieved:
             directions = maze[circle_x][circle_y].directions
             #erasing
             draw_circle(circle_x,circle_y, x_size, y_size, radius, erase,screen,thickness,additional_x,additional_y)
@@ -211,4 +245,29 @@ while True:
                 if Direction.DOWN in directions: 
                     circle_y+=1
             draw_circle(circle_x,circle_y, x_size, y_size, radius, colors[1],screen,thickness,additional_x,additional_y)
+        elif event.type == pygame.USEREVENT and not objective_achieved:
+            erase_text = last_font.render(bottom_text,True, erase,erase)
+            screen.blit(erase_text,bottom_rectangle) 
+            new_text = None
+            if inspiration_iterations % 10 !=0:
+                bottom_text = suggestion if inspiration_iterations%5==0 else random.choice(inspiration)
+                new_text = inspiration_font.render(bottom_text, True, colors[3], erase)
+                last_font = inspiration_font
+                screen.blit(new_text,inspiration_rectangle)
+                last_rectangle = inspiration_rectangle
+            else: 
+                bottom_text = "Navigate from the red dot to the blue dot"
+                new_text = bottom_font.render(bottom_text, True, colors[3], erase)
+                last_font = bottom_font
+                screen.blit(new_text,bottom_rectangle)
+                last_rectangle = bottom_rectangle
+            inspiration_iterations+=1
+    if (circle_x,circle_y) == (end_x,end_y): 
+        objective_achieved = True 
+        erase_text = last_font.render(bottom_text,True, erase,erase)
+        screen.blit(erase_text,last_rectangle)
+        bottom_text = "Objective achieved! Press + to show path"
+        new_text = bottom_font.render(bottom_text, True, colors[3], erase)
+        screen.blit(new_text,bottom_rectangle)
+        last_rectangle = bottom_rectangle
     pygame.display.update()
